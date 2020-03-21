2f40406e9209654c3e40233d9bfd45c618dab532
wszczawinski <w.k.szczawinski@gmail.com>
2020-03-21 20:48:51 +0100

    feat(signOut): setting signOut

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,5 +1,5 @@
-import app from 'firebase/app'
-import 'firebase/auth'
+import app from 'firebase/app';
+import 'firebase/auth';
 
 const config = {
     apiKey: process.env.REACT_APP_API_KEY,
@@ -10,7 +10,7 @@ const config = {
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_APP_ID
 };
-    
+
 class Firebase {
     constructor() {
         app.initializeApp(config);
@@ -18,25 +18,25 @@ class Firebase {
         this.auth = app.auth();
     }
 
-    doCreateUserWithEmailAndPassword = (email, password) =>{
+    doCreateUserWithEmailAndPassword = (email, password) => {
         this.auth.createUserWithEmailAndPassword(email, password);
-    }
+    };
 
     doSignInWithEmailAndPassword = (email, password) => {
         this.auth.signInWithEmailAndPassword(email, password);
-    }
+    };
 
     doSignOut = () => {
         this.auth.signOut();
-    }
+    };
 
     doPasswordReset = email => {
         this.auth.sendPasswordResetEmail(email);
-    }
+    };
 
-    doPasswordUpdate = password =>{
+    doPasswordUpdate = password => {
         this.auth.currentUser.updatePassword(password);
-    }
+    };
 }
 
 export default Firebase;
